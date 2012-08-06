628acab8320690dfa5d56fedc4facffd7938db60
Josemar Davi Luedke <josemarluedke@gmail.com>
2012-08-06 17:31:22 -0300

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Events4me::Application.config.secret_token = '893297e8aa7e0204b783f001f057825af4c33ccc5c7f1209b4430b70bae6ae04c35cddf34e21586163d5737e89a4e53ba624282cf7262d48850dbc693a2f14ed'
