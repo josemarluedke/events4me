8d5a27a74ac320f480d598f0a164324ccab8c962
Josemar Davi Luedke <josemarluedke@gmail.com>
2012-08-09 19:03:44 -0300

    listing the authorizations in user profile

--- app/models/authorization.rb
+++ app/models/authorization.rb
@@ -2,4 +2,5 @@ class Authorization < ActiveRecord::Base
   belongs_to :user
   attr_accessible :provider, :uid
   validates :provider, :uid, presence: true
+  scope :provider, ->(provider) { where(provider: provider).limit(1) }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Events4me::Application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
-
+  resources :authorizations, only: [:destroy]
   root :to => 'home#index'
 end
