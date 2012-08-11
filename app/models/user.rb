6c3e92ff378baac5cacf74e384d840380caef524
Rafael Barboza <barboza.cardoso@gmail.com>
2012-08-11 15:09:00 -0300

    added user management menu with pic

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   # :token_authenticatable, :confirmable, :lockable and :timeoutable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :token_authenticatable, :omniauthable
 
-  attr_accessible :name, :email, :password, :password_confirmation, :remember_me
+  attr_accessible :name, :email, :password, :image_url, :password_confirmation, :remember_me
   has_many :authorizations, dependent: :destroy
   validates :name, presence: true
 
@@ -12,8 +12,17 @@ class User < ActiveRecord::Base
       if data = session[:omniauth] && session[:omniauth]["info"]
         user.email = data["email"] if data["email"].present?
         user.name = data["name"]
+        user.image_url = data["image"]
         user.authorizations.build(provider: session[:omniauth]['provider'], uid: session[:omniauth]['uid'])
       end
+    end
+  end
+
+  def avatar_url
+    if image_url
+      return image_url
+    else
+      "http://gravatar.com/avatar/#{Digest::MD5.new.update(email)}.jpg?s=50"
     end
   end
 end
