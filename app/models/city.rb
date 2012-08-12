d9a152d714531a24e6421e597e66d65ab09388de
Josemar Davi Luedke <josemarluedke@gmail.com>
2012-08-12 00:05:10 -0300

    changes for rails_admin

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,5 +1,5 @@
 class City < ActiveRecord::Base
   belongs_to :state
-  attr_accessible :name
+  attr_accessible :name, :state_id
   validates :name, :state, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Events4me::Application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   resources :authorizations, only: [:destroy]
 
-  match :me, controller: "users", action: "show"
+  resources :events
+  match :me, controller: "users", action: "show", as: :me
 
   root :to => 'home#index'
 end
