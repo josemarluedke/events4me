5bea0fda7c213a9807f5b99bf9885a987065d80d
Rafael Barboza <barboza.cardoso@gmail.com>
2012-08-12 00:34:06 -0300

    added organization-user relation

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,6 +1,7 @@
 class Organization < ActiveRecord::Base
   belongs_to :place
-  attr_accessible :cnpj, :logo, :name
+  has_and_belongs_to_many :users
+  attr_accessible :cnpj, :logo, :name, :place
   validates :name, :place, presence: true
 
   def serializable_hash(options={})
